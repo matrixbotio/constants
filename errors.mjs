13cf9b61b88d445d7d6edee859983d0193ca3194
KaMeHb-UA <marlock@bassteam.ml>
2021-06-10 03:00:42 +0300

    golang errors generator

--- errors.mjs
+++ errors.mjs
@@ -1,19 +1,20 @@
 import YAML from 'yaml';
 import { readFile, writeFile } from 'fs/promises';
 import { resolve } from 'path';
-import genJs from './generators/errors-js.mjs';
+import js from './generators/errors-js.mjs';
+import go from './generators/errors-go.mjs';
 
 const yamlFile = resolve(process.argv[2], 'errors.yml');
 const yaml = readFile(yamlFile, 'utf8').then(YAML.parse);
 
 // destinations
 const jsFile = resolve(process.cwd(), 'errors.js');
+const goFile = resolve(process.cwd(), 'errors.go');
 
 export default async () => {
     const structure = await yaml;
-    const js = genJs(structure);
-
     await Promise.all([
-        writeFile(jsFile, js),
+        writeFile(jsFile, js(structure)),
+        writeFile(goFile, go(structure))
     ]);
 }

a1b580887c087d78cd8ef2516c0ea1e22fb750e0
KaMeHb-UA <marlock@bassteam.ml>
2021-06-10 03:49:43 +0300

    esm

--- processes/errors.mjs
+++ processes/errors.mjs
@@ -1,8 +1,10 @@
 import YAML from 'yaml';
-import { readFile, writeFile } from 'fs/promises';
+import { promises } from 'fs';
 import { resolve } from 'path';
 import js from '../generators/errors-js.mjs';
 import go from '../generators/errors-go.mjs';
+
+const { readFile, writeFile } = promises;
 
 const yamlFile = resolve(process.env.INPUT_CONFIG_PATH, 'errors.yml');
 const yaml = readFile(yamlFile, 'utf8').then(YAML.parse);

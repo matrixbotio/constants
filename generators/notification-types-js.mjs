89078368bbf1248f04dafb267db06ad636a3c20c
KaMeHb-UA <marlock@bassteam.ml>
2021-11-28 19:39:15 +0200

    fix typings

--- generators/notification-types-js.mjs
+++ generators/notification-types-js.mjs
@@ -3,7 +3,7 @@ export default struct => {
     for(const i in struct) res += `\n    ${struct[i]} = ${i},`;
 	return {
 		'notification-types.js': res.slice(0, -1) + ';\n',
-		'index.d.ts': 'export * from "./notification-types";\n',
+		'index.ts': res.slice(0, -1) + ';\n',
 		'index.mjs': 'export * from "https://raw.githubusercontent.com/matrixbotio/constants/master/notification-types/notification-types.js";\n',
 	}
 }

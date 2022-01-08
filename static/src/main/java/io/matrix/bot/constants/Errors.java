4e350bac3abe256c07b16329f719124c12bc555b
KaMeHb-UA <marlock@bassteam.ml>
2022-01-08 15:22:20 +0200

    Java error n logger improvements
    Co-Authored-By: Nikita Zhdanov <22348978+NikitaZhdanov@users.noreply.github.com>
    
    #25

--- static/src/main/java/io/matrix/bot/constants/Errors.java
+++ static/src/main/java/io/matrix/bot/constants/Errors.java
@@ -5,12 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.matrix.bot.constants.model.Error;
 import io.matrix.bot.constants.model.MatrixException;
 
-import java.io.IOException;
+import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.matrix.bot.constants.Util.formatStackTraceString;
+import static io.matrix.bot.constants.Util.initLog;
 
 public class Errors {
 
@@ -19,11 +20,21 @@ public class Errors {
 	private static final Map<String, Error> errors = new HashMap<>();
 
 	static {
+		Map<String, Error> errors = null;
 		try {
-			final Map<String, Error> errors = new ObjectMapper().readValue(new URL(ERRORS_JSON_URL), new TypeReference<>() {});
+			errors = new ObjectMapper().readValue(new URL(ERRORS_JSON_URL), new TypeReference<>() {});
+		} catch (Exception e) {
+			initLog("Exception initializing errors via URL: " + e);
+			try {
+				initLog("Trying to init errors via file");
+				errors = new ObjectMapper().readValue(new File("errors/errors.json"), new TypeReference<>() {});
+				initLog("Successful errors initialization via file");
+			} catch (Exception e1) {
+				initLog("Exception initializing errors via file: " + e1);
+			}
+		}
+		if (errors != null) {
 			Errors.errors.putAll(errors);
-		} catch (final IOException e) {
-			throw new RuntimeException("Exception fetching Errors configuration JSON", e);
 		}
 	}
 

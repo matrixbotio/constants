2213baf023ca3b8a00c5f1883fc32ccb92e98fc9
Alex Lukashevich <aleksandr.dwt@gmail.com>
2022-08-11 21:35:23 +0300

    Make APIError implementing error interface

--- errors.go
+++ errors.go
@@ -16,6 +16,10 @@ type APIError struct {
 	Stack   string `json:"-"`
 }
 
+func (e APIError) Error() string {
+	return fmt.Sprintf("name: %s; code: %d; message: %s", e.Name, e.Code, e.Message)
+}
+
 func getErrors(url string) map[string]*APIError {
 	storage := make(map[string]*APIError)
 	getJSON(url, &storage)
@@ -26,7 +30,7 @@ func getStack() string {
 	rt := stack.Trace().TrimRuntime()
 	rt = rt[3:]
 	str := fmt.Sprintf("%#v", rt)
-	str = str[1:len(str)-1]
+	str = str[1 : len(str)-1]
 	arr := strings.Split(str, " ")
 	return errStackJoin[1:] + strings.Join(arr, errStackJoin)
 }
@@ -49,5 +53,5 @@ func Error(name string, message ...string) *APIError {
 		}
 		return err(res.Code, res.Name, res.Message)
 	}
-	return err(-1, "ERR_UNKNOWN", "Cannot get error named "+name)
+	return err(-1, "ERR_UNKNOWN", "Cannot get error named "+name+"; original message: "+message[0])
 }
